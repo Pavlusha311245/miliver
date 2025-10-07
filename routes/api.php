41837aafd02c9d834c44e888b6d2a895cd500a5d
Pavel Zavadski <zavadskiy.pavel2002@outlook.com>
2025-10-07 12:18:45 +0200

    Remove unused user route and update User model factory annotation for clarity

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,6 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\ProjectController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('projects', ProjectController::class);
-
-Route::get('/user', static function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
