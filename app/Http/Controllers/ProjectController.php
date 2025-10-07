a6db1492871cca426569497165d2c7070e3a60dc
Pavel Zavadski <zavadskiy.pavel2002@outlook.com>
2025-10-07 11:27:15 +0200

    Add strict types declaration to migration, controller, and model files for improved type safety

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1,14 +1,16 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProjectRequest;
 use App\Http\Resources\ProjectResource;
 use App\Models\Project;
+use Dedoc\Scramble\Attributes\QueryParameter;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Throwable;
 
 /**
  * Controller for handling project-related operations.
@@ -17,12 +19,20 @@ class ProjectController extends Controller
 {
     const int PER_PAGE = 10;
 
+    const string SORT_DIRECTION = 'asc';
+
     /**
      * Display a collection of all projects.
      *
-     * @param Request $request The incoming request.
+     * @param  Request  $request  The incoming request.
      * @return JsonResponse The collection of projects.
+     *
+     * @throws Throwable
      */
+    #[QueryParameter('page', description: 'The page number to retrieve.', default: 1)]
+    #[QueryParameter('sort', description: 'The field to sort by.', default: 'id')]
+    #[QueryParameter('direction', description: 'The sorting direction.', default: self::SORT_DIRECTION, example: 'desc')]
+    #[QueryParameter('per_page', description: 'The number of items per page.', default: self::PER_PAGE, example: 5)]
     public function index(Request $request): JsonResponse
     {
         $response = [];
@@ -32,14 +42,14 @@ class ProjectController extends Controller
         if ($request->has('sort')) {
             $sortField = $request->input('sort');
 
-            $requestSortDirection = $request->has('direction') ? $request->input('direction') : 'asc';
-            $sortDirection = in_array($requestSortDirection, ['asc', 'desc']) ? $requestSortDirection : 'asc';
+            $requestSortDirection = $request->has('direction') ? $request->input('direction') : self::SORT_DIRECTION;
+            $sortDirection = in_array($requestSortDirection, ['asc', 'desc']) ? $requestSortDirection : self::SORT_DIRECTION;
 
             $projects->orderBy($sortField, $sortDirection);
 
             $response['sort'] = [
                 'field' => $request->input('sort'),
-                'direction' => $request->has('direction') ? $request->input('direction') : 'asc',
+                'direction' => $request->has('direction') ? $request->input('direction') : self::SORT_DIRECTION,
             ];
         }
 
@@ -52,8 +62,7 @@ class ProjectController extends Controller
             'last_page' => $projectsPagination->lastPage(),
         ];
 
-        $response['_embedded']['projects'] = ProjectResource::collection($projectsPagination);
-
+        $response['_embedded']['projects'] = $projectsPagination->toResourceCollection();
         $response['_links'] = [
             'self' => [
                 'href' => route('projects.index'),
@@ -79,7 +88,7 @@ class ProjectController extends Controller
     /**
      * Handles the storage of a new project resource.
      *
-     * @param ProjectRequest $request The incoming request containing validated project data.
+     * @param  ProjectRequest  $request  The incoming request containing validated project data.
      * @return JsonResponse The newly created project resource.
      */
     public function store(ProjectRequest $request): JsonResponse
@@ -92,7 +101,7 @@ class ProjectController extends Controller
     /**
      * Displays the specified project resource.
      *
-     * @param Project $project The project instance to be displayed.
+     * @param  Project  $project  The project instance to be displayed.
      * @return JsonResponse The specified project resource.
      */
     public function show(Project $project): JsonResponse
@@ -103,8 +112,8 @@ class ProjectController extends Controller
     /**
      * Updates the specified project resource with validated data.
      *
-     * @param ProjectRequest $request The incoming request containing validated project data.
-     * @param Project $project The project instance to be updated.
+     * @param  ProjectRequest  $request  The incoming request containing validated project data.
+     * @param  Project  $project  The project instance to be updated.
      * @return JsonResponse The updated project resource.
      */
     public function update(ProjectRequest $request, Project $project): JsonResponse
@@ -117,7 +126,7 @@ class ProjectController extends Controller
     /**
      * Deletes the specified project resource.
      *
-     * @param Project $project The project instance to be deleted.
+     * @param  Project  $project  The project instance to be deleted.
      * @return JsonResponse A JSON response indicating the result of the deletion.
      */
     public function destroy(Project $project): JsonResponse
